8fd32cb4281dfbdaf55b2e745432ad5af876cb48
Oeng Mengthong <40915642+oengmengthong@users.noreply.github.com>
2024-07-22 11:32:45 +0700

    config env for app id one signal

--- lib/onesignal_config.dart
+++ lib/onesignal_config.dart
@@ -1,5 +1,7 @@
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:onesignal_flutter/onesignal_flutter.dart';
 
 class OneSignalConfig {
+  String foo = dotenv.get('OneSignal_App_ID');
 
 }
